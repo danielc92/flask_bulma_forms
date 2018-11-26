96c8a191aeccf5ace537065db1627eef1f8449dd
Daniel Corcoran <danielc.92g@gmail.com>
2018-11-27 10:49:29 +1100

    Added customfuncs

--- main.py
+++ main.py
@@ -1,22 +1,25 @@
+"""Imports."""
 from flask import Flask, render_template, request
 from customfuncs import validate
 
+
 app = Flask(__name__, static_folder='static')
 
-@app.route('/')
-def index():
-    return render_template('index.html')
 
-@app.route('/form', methods = ['POST', 'GET'])
+@app.route('/', methods=['POST', 'GET'])
 def form():
+    """The main route function."""
     error = None
-    if request.method == "POST":
-        messages = validate(request.form['firstname'],
-                            request.form['lastname'],
-                            request.form['school'])
-        if len(messages) > 0:
-            error = " ".join(messages)
 
-    return render_template('index.html', error = error)
+    if request.method == "POST":
+        errors = validate(request.form['firstname'],
+                          request.form['lastname'],
+                          request.form['school'])
+        if len(errors) > 0:
+            error = " ".join(errors)
+
+    return render_template('index.html', error=error)
+
+
 if __name__ == '__main__':
     app.run(debug=True)
